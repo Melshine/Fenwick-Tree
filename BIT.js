e5d208562aeb0c27e7757bb54dcc894a208a2897
LeoNiz <leo_nd@hotmail.fr>
2018-11-01 12:27:41 +0100

    Add few comments

--- BIT.js
+++ BIT.js
@@ -2,6 +2,7 @@ function bit(arr){
   var n = arr.length;
   var b = Array(n).fill(0);
   for(let x=1; x<=n; x++){
+    // (x & -x) is the range sum for x
     for(let i=x-(x & -x)+1; i<=x; i++) b[x-1] += arr[i-1];
   }
   return b;
@@ -9,11 +10,12 @@ function bit(arr){
 
 class Bit {
   constructor(arr){
-    this.arr = bit(arr);
-    this.init = arr;
+    this.arr = bit(arr); // BIT array
+    this.init = arr; // input array
     this.length = arr.length;
   }
 
+  // cumulative sum to index x
   sumTo(x){
     if(x == undefined) x = this.length-1;
     var sum = 0; x++;
@@ -24,6 +26,7 @@ class Bit {
     return sum;
   }
 
+  // cumulative inclusive sum from index x to y
   sumFromTo(x,y){
     if(y == undefined) y = this.length - 1;
     return this.sumTo(y) - (x==0?0:this.sumTo(x-1));
@@ -33,6 +36,7 @@ class Bit {
     return this.init[x];
   }
 
+  // add v to init[x], and update bit array
   addTo(x,v){
     this.init[x] += v;
     x++;
@@ -41,6 +45,7 @@ class Bit {
     }
   }
 
+  // replace init[x] value by v, and update bit array
   update(x,v){
     var vv = this.init[x];
     this.init[x] = v;
